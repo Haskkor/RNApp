7a4f0354cf48fd662eea74cdefe2d1ba0e9b0804
jfarnaul <jeremy.farnault@fnzc.co.nz>
2017-12-27 13:10:24 +1300

    Working on the log component. Adding a sortable list

--- src/view/components/QuickLog.tsx
+++ src/view/components/QuickLog.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
-import {Dimensions, Picker, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native'
+import {Picker, StyleSheet, Text, TextInput, TouchableHighlight, TouchableOpacity, View} from 'react-native'
+import * as SortableListView from 'react-native-sortable-listview'
 
 type IProps = {}
 
@@ -7,21 +8,51 @@ type IState = {
   currentReps: string
   currentWeight: string
   currentExercise: string
+  dataLog: any // fixme any
 }
 
+
+// let data = {
+//   hello: { text: 'world' },
+//   how: { text: 'are you' },
+//   test: { text: 123 },
+//   this: { text: 'is' },
+//   a: { text: 'a' },
+//   real: { text: 'real' },
+//   drag: { text: 'drag and drop' },
+//   bb: { text: 'bb' },
+//   cc: { text: 'cc' },
+//   dd: { text: 'dd' },
+//   ee: { text: 'ee' },
+//   ff: { text: 'ff' },
+//   gg: { text: 'gg' },
+//   hh: { text: 'hh' },
+//   ii: { text: 'ii' },
+//   jj: { text: 'jj' },
+//   kk: { text: 'kk' },
+// }
+//
+// let order = Object.keys(data)
+
 class QuickLog extends React.PureComponent<IProps, IState> {
+  order: any // fixme any
 
   constructor() {
     super()
     this.state = {
       currentReps: null,
       currentWeight: null,
-      currentExercise: null
+      currentExercise: null,
+      dataLog: {'test': {text: 'test'}}
     }
   }
 
+  componentDidMount() {
+    this.order = Object.keys(this.state.dataLog)
+  }
+
   render() {
-    const {currentReps, currentWeight, currentExercise} = this.state
+    const {currentReps, currentWeight, currentExercise, dataLog} = this.state
 
     return (
       <View style={styles.container}>
@@ -32,8 +63,8 @@ class QuickLog extends React.PureComponent<IProps, IState> {
               style={{width: 100}}
               selectedValue={currentExercise}
               onValueChange={(itemValue) => this.setState({currentExercise: itemValue})}>
-              <Picker.Item key='ttt' label='ttt' value='ttt'/>
-              <Picker.Item key='aaa' label='aaa' value='aaa'/>
+              <Picker.Item key="ttt" label="ttt" value="ttt"/>
+              <Picker.Item key="aaa" label="aaa" value="aaa"/>
             </Picker>
           </View>
           <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
@@ -66,11 +97,29 @@ class QuickLog extends React.PureComponent<IProps, IState> {
             </Text>
           </TouchableOpacity>
         </View>
-        <ScrollView contentContainerStyle={styles.scrollView}>
-          <Text>Test1</Text>
-          <Text>Test2</Text>
-          <Text>Test3</Text>
-        </ScrollView>
+        <SortableListView
+          style={{flex: 1}}
+          data={dataLog}
+          order={this.order}
+          onRowMoved={(e: any) => { // fixme any
+            this.order.splice(e.to, 0, this.order.splice(e.from, 1)[0])
+            this.forceUpdate()
+          }}
+          renderRow={(row: any) =>
+            <TouchableHighlight
+              underlayColor={'#eee'}
+              style={{
+                padding: 25,
+                backgroundColor: '#F8F8F8',
+                borderBottomWidth: 1,
+                borderColor: '#eee'
+              }}
+              {...this.props.sortHandlers}
+            >
+              <Text>{row.text}</Text>
+            </TouchableHighlight>
+          }
+        />
       </View>
     )
   }
