fc179eb222ea64ba363dbdbeb2a78251f173e19e
Jérémy Farnault <haskkor@gmail.com>
2017-12-28 19:32:15 +1300

    Working on the log

--- src/view/components/QuickLog.tsx
+++ src/view/components/QuickLog.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {Picker, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native'
+import {Picker, StyleSheet, Text, TouchableOpacity, View, Modal, ScrollView} from 'react-native'
 import * as SortableListView from 'react-native-sortable-listview'
 import {Col, Row, Grid} from 'react-native-easy-grid'
 
@@ -10,6 +10,7 @@ type IState = {
   currentWeight: string
   currentMuscle: string
   currentExercise: string
+  showModal: boolean
   dataLog: any
 }
 
@@ -49,6 +50,7 @@ class QuickLog extends React.PureComponent<IProps, IState> {
       currentWeight: null,
       currentExercise: null,
       currentMuscle: null,
+      showModal: false,
       dataLog: {test: {text: 'test'}, test2: {text: 'test2'}, test3: {text: 'test3'}}
     }
   }
@@ -60,85 +62,105 @@ class QuickLog extends React.PureComponent<IProps, IState> {
   }
 
   render() {
-    const {currentReps, currentWeight, currentExercise, dataLog, currentMuscle} = this.state
+    const {currentReps, currentWeight, currentExercise, dataLog, currentMuscle, showModal} = this.state
     return (
       <View style={styles.container}>
-
-
-          <Grid style={{flex: 2, marginTop: 40}}>
-            <Row size={35}>
-              <Col size={25}><Text>Muscle:</Text></Col>
-              <Col size={75} style={{justifyContent: 'center', alignItems: 'center'}}>
-                <Picker
-                  style={{width: 300, height: 400}}
-                  itemStyle={{fontSize: 14}}
-                  selectedValue={currentMuscle}
-                  onValueChange={(itemValue) => this.setState({currentMuscle: itemValue})}>
-                  <Picker.Item key="Bench press inclined dumbells" label="Bench press inclined dumbells" value="Bench press inclined dumbells"/>
-                  <Picker.Item key="tta" label="tta" value="tta"/>
-                  <Picker.Item key="tts" label="tts" value="tts"/>
-                </Picker>
-              </Col>
-            </Row>
-            <Row size={35}>
-              <Col size={25}><Text>Exercise:</Text></Col>
-              <Col size={75} style={{justifyContent: 'center', alignItems: 'center'}}>
-                <Picker
-                  style={{width: 300, height: 400}}
-                  itemStyle={{fontSize: 14}}
-                  selectedValue={currentExercise}
-                  onValueChange={(itemValue) => this.setState({currentExercise: itemValue})}>
-                  <Picker.Item key="aas" label="aas" value="aas"/>
-                  <Picker.Item key="aad" label="aad" value="aad"/>
-                  <Picker.Item key="aaf" label="aaf" value="aaf"/>
-                </Picker>
-              </Col>
-            </Row>
-            <Row size={15}>
-              <Col>
+        <View style={styles.header}>
+          <Text style={styles.title}>Quick Log</Text>
+        </View>
+        <Grid style={{flex: 1, marginTop: 40}}>
+          <Row size={30} style={{margin: 10}}>
+            <Col size={25}><Text>Muscle:</Text></Col>
+            <Col size={75} style={{justifyContent: 'center', alignItems: 'center'}}>
+              <Picker
+                style={{width: 300, height: 400}}
+                itemStyle={{fontSize: 14}}
+                selectedValue={currentMuscle}
+                onValueChange={(itemValue) => this.setState({currentMuscle: itemValue})}>
+                <Picker.Item key="Bench press inclined dumbells" label="Bench press inclined dumbells"
+                             value="Bench press inclined dumbells"/>
+                <Picker.Item key="tta" label="tta" value="tta"/>
+                <Picker.Item key="tts" label="tts" value="tts"/>
+              </Picker>
+            </Col>
+          </Row>
+          <Row size={30} style={{margin: 10}}>
+            <Col size={25}><Text>Exercise:</Text></Col>
+            <Col size={75} style={{justifyContent: 'center', alignItems: 'center'}}>
+              <Picker
+                style={{width: 300, height: 400}}
+                itemStyle={{fontSize: 14}}
+                selectedValue={currentExercise}
+                onValueChange={(itemValue) => this.setState({currentExercise: itemValue})}>
+                <Picker.Item key="aas" label="aas" value="aas"/>
+                <Picker.Item key="aad" label="aad" value="aad"/>
+                <Picker.Item key="aaf" label="aaf" value="aaf"/>
+              </Picker>
+            </Col>
+          </Row>
+          <Row size={20} style={{margin: 10}}>
+            <ScrollView horizontal={true}>
+              <Col style={{justifyContent: 'center', alignItems: 'center'}}>
                 <TouchableOpacity style={{flexDirection: 'column'}}>
-                  <Text>8 x</Text>
-                  <Text>75 kg</Text>
+                  <Text><Text>8</Text><Text> x</Text></Text>
+                  <Text><Text>75</Text><Text>kg</Text></Text>
                 </TouchableOpacity>
               </Col>
-              <Col>
+              <Col style={{justifyContent: 'center', alignItems: 'center'}}>
                 <TouchableOpacity style={{flexDirection: 'column'}}>
-                  <Text>8 x</Text>
-                  <Text>75 kg</Text>
+                  <Text><Text>8</Text><Text> x</Text></Text>
+                  <Text><Text>75</Text><Text>kg</Text></Text>
                 </TouchableOpacity>
               </Col>
-              <Col>
+              <Col style={{justifyContent: 'center', alignItems: 'center'}}>
                 <TouchableOpacity style={{flexDirection: 'column'}}>
-                  <Text>8 x</Text>
-                  <Text>75 kg</Text>
+                  <Text><Text>8</Text><Text> x</Text></Text>
+                  <Text><Text>75</Text><Text>kg</Text></Text>
                 </TouchableOpacity>
               </Col>
-            </Row>
-            <Row size={15}>
-              <Col>
-                <TouchableOpacity
-                  style={{flex: 1}}
-                  onPress={() => console.log('test')}>
-                  <Text>
-                    Done
-                  </Text>
+              <Col style={{justifyContent: 'center', alignItems: 'center'}}>
+                <TouchableOpacity style={{flexDirection: 'column'}}>
+                  <Text><Text>8</Text><Text> x</Text></Text>
+                  <Text><Text>75</Text><Text>kg</Text></Text>
                 </TouchableOpacity>
               </Col>
-            </Row>
-
-          </Grid>
-
-
-        <SortableListView
-          style={{flex: 1}}
-          data={dataLog}
-          order={this.order}
-          onRowMoved={(e: any) => {
-            this.order.splice(e.to, 0, this.order.splice(e.from, 1)[0])
-            this.forceUpdate()
-          }}
-          renderRow={(row: DataRow) => <RowComponent data={row}/>}
-        />
+            </ScrollView>
+          </Row>
+          <Row size={10} style={{margin: 10}}>
+            <Col style={{justifyContent: 'center', alignItems: 'center'}}>
+              <TouchableOpacity
+                onPress={() => console.log('test')}>
+                <Text>
+                  Add
+                </Text>
+              </TouchableOpacity>
+            </Col>
+          </Row>
+          <Row size={10} style={{margin: 10}}>
+            <Col style={{justifyContent: 'center', alignItems: 'center'}}>
+              <TouchableOpacity
+                onPress={() => this.setState({showModal: true})}>
+                <Text>
+                  See current training
+                </Text>
+              </TouchableOpacity>
+            </Col>
+          </Row>
+        </Grid>
+        <Modal
+          visible={showModal}
+          animationType="slide">
+          <SortableListView
+            style={{flex: 1}}
+            data={dataLog}
+            order={this.order}
+            onRowMoved={(e: any) => {
+              this.order.splice(e.to, 0, this.order.splice(e.from, 1)[0])
+              this.forceUpdate()
+            }}
+            renderRow={(row: DataRow) => <RowComponent data={row}/>}
+          />
+        </Modal>
       </View>
     )
   }
@@ -147,6 +169,18 @@ class QuickLog extends React.PureComponent<IProps, IState> {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  header: {
+    borderBottomWidth: 0.5,
+    borderColor: '#414143',
+    paddingTop: 30,
+    paddingBottom: 20,
+    backgroundColor: '#282829'
+  },
+  title: {
+    alignSelf: 'center',
+    fontWeight: '600',
+    color: '#FFF'
   },
   logView: {
     flex: 2,
