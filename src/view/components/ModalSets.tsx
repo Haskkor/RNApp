0ba5f415068a2199437c493e3e6073c1f7625b84
New Author Name <haskkor@gmail.com>
2018-01-04 12:53:51 +1300

    Styling the sets modal

--- src/view/components/ModalSets.tsx
+++ src/view/components/ModalSets.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {View, Modal, Picker, StyleSheet, Dimensions, Text} from 'react-native'
+import {View, Modal, Picker, StyleSheet, Text, TouchableOpacity, Dimensions} from 'react-native'
 import {Grid, Row, Col} from 'react-native-easy-grid'
 import * as loDash from 'lodash'
 
@@ -8,6 +8,7 @@ type IProps = {
   modifySet: (weight: number, reps: number) => void
   reps: number
   weight: number
+  closeModal: () => void
 }
 
 type IState = {
@@ -26,49 +27,63 @@ class ModalSets extends React.PureComponent<IProps, IState> {
     const {currentReps, currentWeight} = this.state
     return (
       <View>
-      <Modal
-        onRequestClose={() => console.log('close')}
-        visible={true}
-        transparent={true}
-        animationType="slide">
-        <View style={styles.container}>
-          <View style={styles.viewOpacity}/>
-          <View style={styles.viewPickers}>
-            <Grid>
-              <Col>
-                <Row>
-                  <Text>Reps:</Text>
-                </Row>
-                <Row>
-                  <Picker
-                    style={styles.picker}
-                    selectedValue={currentReps}
-                    onValueChange={(itemValue) => this.setState({currentReps: itemValue})}>
-                    {loDash.range(1, 30).map((value: number) => {
-                      return <Picker.Item key={value} label={value.toString()} value={value}/>
-                    })}
-                  </Picker>
-                </Row>
-              </Col>
-              <Col>
-                <Row>
-                  <Text>Weight:</Text>
-                </Row>
-                <Row>
-                  <Picker
-                    style={styles.picker}
-                    selectedValue={currentWeight}
-                    onValueChange={(itemValue) => this.setState({currentWeight: itemValue})}>
-                    {loDash.range(1, 500).map((value: number) => {
-                      return <Picker.Item key={value} label={value.toString()} value={value}/>
-                    })}
-                  </Picker>
-                </Row>
-              </Col>
-            </Grid>
+        <Modal
+          onRequestClose={() => console.log('close')}
+          visible={true}
+          transparent={true}
+          animationType="slide">
+          <View style={styles.container}>
+            <View style={styles.viewOpacity}/>
+            <View style={styles.viewPickers}>
+              <View style={styles.viewButtons}>
+                <TouchableOpacity
+                  style={styles.buttonDelete}
+                  onPress={() => this.props.closeModal()}>
+                  <Text style={styles.textDelete}>Delete</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.buttonSave}
+                  onPress={() => this.props.closeModal()}>
+                  <Text>Save</Text>
+                </TouchableOpacity>
+              </View>
+              <Grid style={styles.grid}>
+                <Col style={{justifyContent: 'center', alignItems: 'center'}}>
+                  <Row size={10}>
+                    <Text>Reps:</Text>
+                  </Row>
+                  <Row size={90}>
+                    <Picker
+                      style={styles.picker}
+                      itemStyle={{fontSize: 14}}
+                      selectedValue={currentReps}
+                      onValueChange={(itemValue) => this.setState({currentReps: itemValue})}>
+                      {loDash.range(1, 30).map((value: number) => {
+                        return <Picker.Item key={value} label={value.toString()} value={value}/>
+                      })}
+                    </Picker>
+                  </Row>
+                </Col>
+                <Col style={{justifyContent: 'center', alignItems: 'center'}}>
+                  <Row size={10}>
+                    <Text>Weight:</Text>
+                  </Row>
+                  <Row size={90}>
+                    <Picker
+                      style={styles.picker}
+                      itemStyle={{fontSize: 14}}
+                      selectedValue={currentWeight}
+                      onValueChange={(itemValue) => this.setState({currentWeight: itemValue})}>
+                      {loDash.range(1, 500).map((value: number) => {
+                        return <Picker.Item key={value} label={value.toString()} value={value}/>
+                      })}
+                    </Picker>
+                  </Row>
+                </Col>
+              </Grid>
+            </View>
           </View>
-        </View>
-      </Modal>
+        </Modal>
       </View>
     )
   }
@@ -76,7 +91,7 @@ class ModalSets extends React.PureComponent<IProps, IState> {
 
 const styles = StyleSheet.create({
   picker: {
-    width: 250,
+    width: 100,
     height: 'auto'
   },
   container: {
@@ -85,11 +100,33 @@ const styles = StyleSheet.create({
   viewOpacity: {
     backgroundColor: 'black',
     opacity: 0.5,
-    flex: 2
+    flex: 1
   },
   viewPickers: {
     flex: 1,
     backgroundColor: '#F7F7F8'
+  },
+  viewButtons: {
+    flexDirection: 'row',
+    backgroundColor: '#EFEFF4',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1
+  },
+  buttonDelete: {
+    position: 'absolute',
+    left: 20
+  },
+  buttonSave: {
+    position: 'absolute',
+    right: 20
+  },
+  grid: {
+    padding: 20
+  },
+  textDelete: {
+    color: 'red'
   }
 })
 
